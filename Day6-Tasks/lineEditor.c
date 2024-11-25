c3da8da4a81531021f226b689e22cad3f58d052a
Mostafa <mostafamohammmed82@gmail.com>
2024-11-25 21:33:47 +0200

    switch to pointers

--- Day6-Tasks/lineEditor.c
+++ Day6-Tasks/lineEditor.c
@@ -10,127 +10,105 @@
 #define END 70
 #define HOME 72
 
-int main(){
-    char content[SIZE] ={0};
-    char cursorPosition = 0;
-    char currentSize = 0;
+int main() {
+    char* content[SIZE] = {0};
+    char** cursorPosition = content;
+    char** currentPosition = content;
     char insertFlag = 0;
 
-
-    while(1){
-        if(insertFlag == 0){
+    while (1) {
+        if (insertFlag == 0) {
             printf("\033[6 q");
-
-        }
-        else{
+        } else {
             printf("\033[4 q");
         }
 
-
         char ch = getch();
         if (ch == EXTEND) {
-           if (!kbhit()) {
-               clrscr();
-               return 0;
-           }
-        ch = getch();
-        ch = getch();
+            if (!kbhit()) {
+                clrscr();
+                return 0;
+            }
+            ch = getch();
+            ch = getch();
         }
 
-
-        switch (ch)
-        {
-        case RIGHT :
-            cursorPosition++;
-            if(cursorPosition >= currentSize){
-                cursorPosition = currentSize;
-            }
-            break;
-
-        case END :
-            cursorPosition = currentSize;
-            break;
-
-        case HOME :
-            cursorPosition = 0;
-            break;
-
-        case LEFT :
-            cursorPosition--;
-            if(cursorPosition <= 0){
-                cursorPosition = 0;
-            }
-            break;
-
-        case INSERT:
-            if(insertFlag == 0){
-                insertFlag = 1;
-            }
-            else{
-                insertFlag = 0;
-            }
-            break;
-
-        case BACK:
-            if(cursorPosition == 0){
-                break;
-            }
-            for (int i = cursorPosition; i < currentSize; i++) {
-                        content[i-1] = content[i];
-            }
-            content[currentSize] = '\0';
-            currentSize--;
-            cursorPosition--;
-            break;
-
-        case DELETE:
-
-            if(cursorPosition == currentSize){
-                break;
-            }
-            for (int i = cursorPosition; i < currentSize; i++) {
-                        content[i] = content[i+1];
-            }
-            content[currentSize] = '\0';
-            currentSize--;
-            break;
-
-        default:
-        if(ch >= 32 && ch <= 126){
-            if (currentSize == cursorPosition){
-                content[currentSize] = ch;
-                currentSize++;
-                cursorPosition++;
-            }
-            else{
-                    if(insertFlag == 0){
-
-                for (int i = currentSize; i > cursorPosition; i--) {
-                        content[i] = content[i - 1];
-                    }
-
-                content[cursorPosition] = ch;
-                currentSize++;
-                cursorPosition++;
-            }
-            else{
-                content[cursorPosition] = ch;
-                currentSize++;
-                cursorPosition++;
+        switch (ch) {
+            case RIGHT:
+                if (cursorPosition < currentPosition) {
+                    cursorPosition++;
                 }
-            }
+                break;
 
-            break;
-        }
+            case END:
+                cursorPosition = currentPosition;
+                break;
+
+            case HOME:
+                cursorPosition = content;
+                break;
+
+            case LEFT:
+                if (cursorPosition > content) {
+                    cursorPosition--;
+                }
+                break;
+
+            case INSERT:
+                insertFlag = !insertFlag;
+                break;
+
+            case BACK:
+                if (cursorPosition > content) {
+                    for (char** ptr = cursorPosition - 1; ptr < currentPosition - 1; ptr++) {
+                        *ptr = *(ptr + 1);
+                    }
+                    *(currentPosition - 1) = 0;
+                    currentPosition--;
+                    cursorPosition--;
+                }
+                break;
+
+            case DELETE:
+                if (cursorPosition < currentPosition) {
+                    for (char** ptr = cursorPosition; ptr < currentPosition - 1; ptr++) {
+                        *ptr = *(ptr + 1);
+                    }
+                    *(currentPosition - 1) = 0;
+                    currentPosition--;
+                }
+                break;
+
+            default:
+                if (ch >= 32 && ch <= 126) {
+                    if (cursorPosition == currentPosition) {
+                        *cursorPosition = ch;
+                        currentPosition++;
+                        cursorPosition++;
+                    } else {
+                        if (insertFlag == 0) {
+                            for (char** ptr = currentPosition; ptr > cursorPosition; ptr--) {
+                                *ptr = *(ptr - 1);
+                            }
+                            *cursorPosition = ch;
+                            currentPosition++;
+                            cursorPosition++;
+                        } else {
+                            *cursorPosition = ch;
+                            cursorPosition++;
+                        }
+                    }
+                }
+                break;
         }
 
         clrscr();
-        for(int i = 0;i<currentSize;i++){
-            printf("%c",content[i]);
+        for (char** ptr = content; ptr < currentPosition; ptr++) {
+            printf("%c", *ptr);
         }
 
-        printf("\033[;%dH", cursorPosition+1);
-}
+        printf("\033[;%dH", (cursorPosition - content) + 1);
+    }
 
     return 0;
 }
